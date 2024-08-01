d093f627ced3fe1104bc4e79ad55a28d5ce0833e
kevtico20 <50472439+kevtico20@users.noreply.github.com>
2024-07-31 21:35:52 -0600

    Se agrego el diseño de la ventana de juego la parte del menu

--- src/main/java/una/cr/ac/model/LinkedList.java
+++ src/main/java/una/cr/ac/model/LinkedList.java
@@ -75,7 +75,7 @@ public class LinkedList {
         switch (level) {
             case 1:
                 levelData = new String[]{
-                    "################",
+                    "####################",
                     "#@",
                     "#",
                     "#",
