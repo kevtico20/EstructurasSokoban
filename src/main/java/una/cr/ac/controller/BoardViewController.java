0eedafa4f52b9d184b7bed21db0c03673ff18941
Joksanmj72 <joksanmj.777@gmail.com>
2024-08-03 16:42:34 -0600

    CHARACTER  ANIMATION

--- src/main/java/una/cr/ac/controller/BoardViewController.java
+++ src/main/java/una/cr/ac/controller/BoardViewController.java
@@ -3,6 +3,9 @@ package una.cr.ac.controller;
 import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -18,6 +21,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 import una.cr.ac.model.Img;
 import una.cr.ac.model.LevelManager;
 import una.cr.ac.model.LinkedList;
@@ -56,6 +60,9 @@ public class BoardViewController extends Controller implements Initializable {
 
     private LevelManager levelManager;
 
+    int direction = 0;
+    int count = 0;
+
     /**
      * Initializes the controller class.
      */
@@ -84,36 +91,51 @@ public class BoardViewController extends Controller implements Initializable {
 
     @FXML
     private void moveUp(ActionEvent event) {
+        direction = 1;
         movePlayer(levelManager.getPlayer().getX(), levelManager.getPlayer().getY() - 1);
+        
+
     }
 
     @FXML
     private void moveDown(ActionEvent event) {
+        direction= 0;
         movePlayer(levelManager.getPlayer().getX(), levelManager.getPlayer().getY() + 1);
+        
+
     }
 
     @FXML
     private void moveLeft(ActionEvent event) {
+        direction=2;
         movePlayer(levelManager.getPlayer().getX() - 1, levelManager.getPlayer().getY());
+        
+
     }
 
     @FXML
     private void moveRight(ActionEvent event) {
+        direction=3;
         movePlayer(levelManager.getPlayer().getX() + 1, levelManager.getPlayer().getY());
+
     }
 
     private void handleKeyPress(KeyEvent event) {
         switch (event.getCode()) {
             case W:
+                direction=1;
                 movePlayer(levelManager.getPlayer().getX(), levelManager.getPlayer().getY() - 1);
                 break;
             case S:
+                direction=0;
                 movePlayer(levelManager.getPlayer().getX(), levelManager.getPlayer().getY() + 1);
                 break;
             case A:
+                direction=2;
                 movePlayer(levelManager.getPlayer().getX() - 1, levelManager.getPlayer().getY());
                 break;
             case D:
+                direction=3;
                 movePlayer(levelManager.getPlayer().getX() + 1, levelManager.getPlayer().getY());
                 break;
             default:
@@ -126,6 +148,11 @@ public class BoardViewController extends Controller implements Initializable {
             levelManager.movePlayer(newX, newY);
             drawBoard();
             System.out.println(levelManager.getBoard());
+             if(count==3){
+                    count=0;
+                } else{
+                count++;
+              }
         } else {
             System.out.println("Movimiento no válido: (" + newX + ", " + newY + ")");
         }
@@ -158,45 +185,67 @@ public class BoardViewController extends Controller implements Initializable {
             for (int j = 0; j < levelManager.getBoard().getWidth(); j++) {
                 char cell = levelManager.getBoard().getCell(j, i);
                 String imagePath = "";
+                ImageView imageView = null;
 
                 switch (cell) {
                     case '#':
                         imagePath = "/una/cr/ac/sokoban/resources/wall.png";
+                        imageView = new ImageView(new Image(getClass().getResourceAsStream(imagePath), 40, 40, false, true));
                         break;
                     case '.':
                         imagePath = "/una/cr/ac/sokoban/resources/point.png";
+                        imageView = new ImageView(new Image(getClass().getResourceAsStream(imagePath), 40, 40, false, true));
                         break;
                     case '@':
-                        imagePath = "/una/cr/ac/sokoban/resources/foto1.png";
+                        // Implementar animación para el personaje '@'
+                        imageView = createAnimatedSprite();
                         break;
                     case '$':
                         imagePath = "/una/cr/ac/sokoban/resources/box.png";
+                        imageView = new ImageView(new Image(getClass().getResourceAsStream(imagePath), 40, 40, false, true));
                         break;
                     case '!':
                         imagePath = "/una/cr/ac/sokoban/resources/boxPoint.png";
+                        imageView = new ImageView(new Image(getClass().getResourceAsStream(imagePath), 40, 40, false, true));
                         break;
                     case ' ':
                         imagePath = "/una/cr/ac/sokoban/resources/floor.png";
+                        imageView = new ImageView(new Image(getClass().getResourceAsStream(imagePath), 40, 40, false, true));
                         break;
                     default:
                         continue; // Si no hay imagen para este carácter, continuar
                 }
 
-                Image image = new Image(getClass().getResourceAsStream(imagePath), 40, 40, false, true);
-                if (image.isError()) {
-                    System.out.println("Error al cargar la imagen: " + image.getException());
-                } else {
-                    ImageView imageView = new ImageView(image);
-                    imageView.setX(j * 40);
-                    imageView.setY(i * 40);
-                    imageView.setFitWidth(40);
-                    imageView.setFitHeight(40);
-                    gridPane.add(imageView, j, i);
+                if (imageView != null) {
+                    gridPane.add(imageView, j, i); // Agregar ImageView a GridPane en la posición (columna, fila)
                 }
             }
         }
 
         updateLevelLabel();
+    }
+
+    private ImageView createAnimatedSprite() {
+        // Configuración del sprite sheet
+        Image spriteSheet = new Image(getClass().getResourceAsStream("/una/cr/ac/sokoban/resources/frames.png"));
+        ImageView imageView = new ImageView(spriteSheet);
+        imageView.setFitWidth(40);
+        imageView.setFitHeight(40);
+
+        // Variables para la animación
+        final int[] currentFrame = {0};
+        final int row = direction;
+        final int WIDTH = 160;
+        final int HEIGHT = 160;
+        
+
+        // Crear la animación
+        currentFrame[0] = (currentFrame[0] + count) % 4;
+        int x = currentFrame[0] * WIDTH;
+        int y = row * HEIGHT;
+        imageView.setViewport(new javafx.geometry.Rectangle2D(x, y, WIDTH, HEIGHT));
+
+        return imageView;
     }
 
     @FXML
